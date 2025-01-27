d5ae762ddc6bf017776ec88c7216dc174863e726
David Dvali <david.dvali@wolterskluwer.com>
2025-01-27 22:13:24 +0400

    Add new tables: Contracts, ContractDetails, Transactions.

---
+++ Warehouse.Database/dbo/Tables/ContractDetails.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[ContractDetails]
+(
+    ContractDetailID INT PRIMARY KEY IDENTITY(1,1),
+    ContractID INT NOT NULL REFERENCES Contracts(ContractID),
+    SlotID INT NOT NULL REFERENCES Slots(SlotID),
+    StartDate DATETIME NOT NULL,
+    EndDate DATETIME NOT NULL,
+)
