9fc9447417f0f75e515335a9fdb5e319b16606f3
Giorgi Kapanadze <giorgi.kapanadze.2001@inbox.ru>
2025-02-03 16:47:05 +0400

    Add Users CRUD Procedures

---
+++ Warehouse.Database/dbo/Stored Procedures/sp_InsertUser.sql
@@ -0,0 +1,15 @@
+﻿CREATE PROCEDURE sp_InsertUser
+    @UserName VARCHAR(30),
+	@Password VARBINARY(64),
+	@UserRole TINYINT,
+	@UserID INT OUT
+AS
+BEGIN
+     SET NOCOUNT ON;
+
+	 INSERT INTO Users(Username, Password, UserRole)
+	 VALUES(@UserName, @Password, @UserRole)
+
+	 SET @UserID = SCOPE_IDENTITY()
+     RETURN 0;
+END
---
+++ Warehouse.Database/dbo/Stored Procedures/sp_UpdateUser.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE sp_UpdateUser
+    @UserID INT,
+	@Username VARCHAR(30),
+	@Password VARBINARY(64),
+	@UserRole TINYINT
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+	IF EXISTS(SELECT 1 FROM Users WHERE UserID = @UserID AND IsActive = 0)
+	BEGIN 
+	     RAISERROR('Record is not active', 16, 1)
+		 RETURN 1;
+	END
+
+	UPDATE Users
+	SET Username = @UserName,
+	    Password = @Password,
+		UserRole = @UserRole
+	WHERE UserID = @UserID AND (Username != @Username OR Password != @Password OR UserRole != @UserRole)
+
+	RETURN 0;
+END
