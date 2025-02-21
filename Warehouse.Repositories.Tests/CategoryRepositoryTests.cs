0c08522732b1884799d9b7d76e3cdadd51d034bf
kaxaniku <kaxaniku@gmail.com>
2025-02-21 18:56:00 +0400

    Tested the rest of CRUD operations for Category.

--- Warehouse.Repositories.Tests/CategoryRepositoryTests.cs
+++ Warehouse.Repositories.Tests/CategoryRepositoryTests.cs
@@ -27,8 +27,42 @@ namespace Warehouse.Repositories.Tests
                 Name = "Test Category",
                 Description = "Test Description"
             };
+
             int id = (int)repository.Insert(category);
             Assert.Greater(id, 0);
+        }
+
+        [Test]
+        public void TestUpdate_ShouldUpdate()
+        {
+            CategoryRepository repository = new(_connection);
+            Category category = new()
+            {
+                CategoryId = 5,
+                Name = "Updated Category",
+                Description = "Test Description was updated"
+            };
+
+            repository.Update(category);
+        }
+
+        [Test]
+        public void TestGet_ShouldGet()
+        {
+            CategoryRepository repository = new(_connection);
+            int id = 6;
+
+            Category? result = repository.Get(id);
+            Console.WriteLine(result!.Name);
+        }
+
+        [Test]
+        public void TestDelete_ShouldDelete()
+        {
+            CategoryRepository repository = new(_connection);
+            int id = 7;
+
+            repository.Delete(id);
         }
     }
 }
