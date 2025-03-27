8d243adb20a2e3a85fe3892460d5cf5be11ff7b0
David Dvali <david.dvali@gmail.com>
2025-03-27 20:54:01 +0400

    Add ProductService and IProductService interface.

---
+++ Warehouse.Services/Interfaces/Services/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using Warehouse.DTO;
+
+namespace Warehouse.Services.Interfaces.Services;
+
+public interface IProductService
+{
+    void AddCategory(Category category);
+    void EditCategory(Category category);
+    void DeleteCategory(int id);
+    Category GetCategory(int id);
+    IEnumerable<Category> GetCategories();
+    void AddProduct(Product product);
+    void EditProduct(Product product);
+    void DeleteProduct(int id);
+    Product GetProduct(int id);
+    IEnumerable<Product> GetProducts();
+}
---
+++ Warehouse.Services/ProductService.cs
@@ -0,0 +1,64 @@
+﻿using Warehouse.DTO;
+using Warehouse.Services.Interfaces.Repositories;
+using Warehouse.Services.Interfaces.Services;
+
+namespace Warehouse.Services;
+
+public class ProductService : IProductService
+{
+    private readonly IUnitOfWork _unitOfWork;
+
+    public ProductService(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
+    }
+
+    public void AddCategory(Category category)
+    {
+        ArgumentNullException.ThrowIfNull(category, nameof(category));
+        _unitOfWork.CategoryRepository.Insert(category);
+    }
+
+    public void EditCategory(Category category)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteCategory(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Category> GetCategories()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Category GetCategory(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void AddProduct(Product product)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void EditProduct(Product product)
+    {
+        throw new NotImplementedException();
+    }
+    public void DeleteProduct(int id)
+    {
+        throw new NotImplementedException();
+    }
+    public Product GetProduct(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Product> GetProducts()
+    {
+        throw new NotImplementedException();
+    }
+}
