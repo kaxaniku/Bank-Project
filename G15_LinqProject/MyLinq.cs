9fae2c1481fbd4bfb194aa72c205b1935f7d9900
kaxaniku <kaxaniku@gmail.com>
2025-05-14 14:00:33 +0400

    Implement LINQ-like methods.

--- G15_LinqProject/MyLinq.cs
+++ G15_LinqProject/MyLinq.cs
@@ -1,4 +1,6 @@
-﻿namespace G15_LinqProject;
+﻿using System;
+
+namespace G15_LinqProject;
 
 static class MyLinq
 {
@@ -18,17 +20,55 @@ static class MyLinq
 
     public static bool MyAny<T>(this IEnumerable<T> source, Func<T, bool> predicate)
     {
-        throw new NotImplementedException();
+        ArgumentNullException.ThrowIfNull(source, nameof(source));
+        ArgumentNullException.ThrowIfNull(predicate, nameof(predicate));
+
+        foreach (T item in source)
+        {
+            if (predicate(item))
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     public static bool MyAll<T>(this IEnumerable<T> source, Func<T, bool> predicate)
     {
-        throw new NotImplementedException();
+        ArgumentNullException.ThrowIfNull(source, nameof(source));
+        ArgumentNullException.ThrowIfNull(predicate, nameof(predicate));
+
+        foreach (T item in source)
+        {
+            if (predicate(item))
+            {
+                continue;
+            }
+            return false;
+        }
+
+        return true;
     }
 
     public static IEnumerable<T> MyConcat<T>(this IEnumerable<T> list1, IEnumerable<T> list2)
     {
-        throw new NotImplementedException();
+        ArgumentNullException.ThrowIfNull(list1, nameof(list1));
+        ArgumentNullException.ThrowIfNull(list2, nameof(list2));
+
+        List<T> result = new List<T>();
+
+        foreach (T item in list1)
+        {
+            result.Add(item);
+        }
+
+        foreach (T item in list2)
+        {
+            result.Add(item);
+        }
+
+        return result;
     }
 
     public static IEnumerable<T> MyUnion<T>(this IEnumerable<T> list1, IEnumerable<T> list2)
@@ -43,7 +83,27 @@ static class MyLinq
 
     public static IEnumerable<T> MyExcept<T>(this IEnumerable<T> list1, IEnumerable<T> list2)
     {
-        throw new NotImplementedException();
+        ArgumentNullException.ThrowIfNull(list1, nameof(list1));
+        ArgumentNullException.ThrowIfNull(list2, nameof(list2));
+
+        List<T> result = new List<T>();
+
+        foreach (T item1 in list1)
+        {
+            bool found = false;
+
+            foreach (var item2 in list2)
+            {
+                if (item1!.Equals(item2))
+                {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) result.Add(item1);
+        }
+
+        return result;
     }
 
     public static IEnumerable<T> MyDistinct<T>(this IEnumerable<T> source)
--- G15_LinqProject/Program.cs
+++ G15_LinqProject/Program.cs
@@ -3,28 +3,28 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        var products = ProductProvider.GetProducts();
+        //var products = ProductProvider.GetProducts();
 
-        int pageSize = 20;
-        int pageNumber = 1;
-        int totalProducts = products.Count();
-        int totalPages = (int)Math.Ceiling((double)totalProducts / pageSize);
+        //int pageSize = 20;
+        //int pageNumber = 1;
+        //int totalProducts = products.Count();
+        //int totalPages = (int)Math.Ceiling((double)totalProducts / pageSize);
 
-        while (pageNumber <= totalPages)
-        {
-            var paginatedProducts = products
-                .Skip((pageNumber - 1) * pageSize)
-                .Take(pageSize);
-            Console.WriteLine($"Page {pageNumber} of {totalPages}:");
-            foreach (var product in paginatedProducts)
-            {
-                Console.WriteLine(product);
-            }
-            pageNumber++;
+        //while (pageNumber <= totalPages)
+        //{
+        //    var paginatedProducts = products
+        //        .Skip((pageNumber - 1) * pageSize)
+        //        .Take(pageSize);
+        //    Console.WriteLine($"Page {pageNumber} of {totalPages}:");
+        //    foreach (var product in paginatedProducts)
+        //    {
+        //        Console.WriteLine(product);
+        //    }
+        //    pageNumber++;
 
-            Console.WriteLine("Press any key to continue to the next page...");
-            Console.ReadKey(true);
-            Console.Clear();
-        }
+        //    Console.WriteLine("Press any key to continue to the next page...");
+        //    Console.ReadKey(true);
+        //    Console.Clear();
+        //}
     }
 }
