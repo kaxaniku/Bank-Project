32d3a80c1d49b746b64e1324fb1e76a266e7b591
ilia-mamniashvili <iliamamniashvili02@gmail.com>
2025-05-19 19:44:21 +0400

    different methods for MyLINQ

--- G15_LinqProject/MyLinq.cs
+++ G15_LinqProject/MyLinq.cs
@@ -9,13 +9,24 @@ static class MyLinq
         ArgumentNullException.ThrowIfNull(source, nameof(source));
         ArgumentNullException.ThrowIfNull(predicate, nameof(predicate));
 
-        foreach (T item in source)
+        using var enumerator = source.GetEnumerator();
+
+        while (enumerator.MoveNext())
         {
+            var item = enumerator.Current;
             if (predicate(item))
             {
                 yield return item;
             }
         }
+
+        //foreach (T item in source)
+        //{
+        //    if (predicate(item))
+        //    {
+        //        yield return item;
+        //    }
+        //}
     }
 
     public static bool MyAny<T>(this IEnumerable<T> source, Func<T, bool> predicate)
@@ -23,13 +34,24 @@ static class MyLinq
         ArgumentNullException.ThrowIfNull(source, nameof(source));
         ArgumentNullException.ThrowIfNull(predicate, nameof(predicate));
 
-        foreach (T item in source)
+        using var enumerator = source.GetEnumerator();
+
+        while (enumerator.MoveNext())
         {
+            var item = enumerator.Current;
             if (predicate(item))
             {
                 return true;
             }
         }
+        
+        //foreach (T item in source)
+        //{
+        //    if (predicate(item))
+        //    {
+        //        return true;
+        //    }
+        //}
 
         return false;
     }
@@ -39,14 +61,26 @@ static class MyLinq
         ArgumentNullException.ThrowIfNull(source, nameof(source));
         ArgumentNullException.ThrowIfNull(predicate, nameof(predicate));
 
-        foreach (T item in source)
+        var enumerator = source.GetEnumerator();
+
+        while (enumerator.MoveNext())
         {
+            var item = enumerator.Current;
             if (predicate(item))
             {
                 continue;
             }
             return false;
         }
+
+        //foreach (T item in source)
+        //{
+        //    if (predicate(item))
+        //    {
+        //        continue;
+        //    }
+        //    return false;
+        //}
 
         return true;
     }
@@ -56,19 +90,31 @@ static class MyLinq
         ArgumentNullException.ThrowIfNull(list1, nameof(list1));
         ArgumentNullException.ThrowIfNull(list2, nameof(list2));
 
-        List<T> result = new List<T>();
-
-        foreach (T item in list1)
+        using var enumerator1 = list1.GetEnumerator();
+        while (enumerator1.MoveNext())
         {
-            result.Add(item);
+            yield return enumerator1.Current;
         }
 
-        foreach (T item in list2)
+        using var enumerator2 = list2.GetEnumerator();
+        while (enumerator2.MoveNext())
         {
-            result.Add(item);
+            yield return enumerator2.Current;
         }
 
-        return result;
+        //List<T> result = new List<T>();
+
+        //foreach (T item in list1)
+        //{
+        //    result.Add(item);
+        //}
+
+        //foreach (T item in list2)
+        //{
+        //    result.Add(item);
+        //}
+
+        //return result;
     }
 
     public static IEnumerable<T> MyUnion<T>(this IEnumerable<T> list1, IEnumerable<T> list2)
@@ -76,21 +122,37 @@ static class MyLinq
         ArgumentNullException.ThrowIfNull(list1, nameof(list1));
         ArgumentNullException.ThrowIfNull(list2, nameof(list2));
 
-        List<T> result = new List<T>();
-        foreach (T item in list1)
+        HashSet<T> seen = new HashSet<T>();
+
+        using var enumerator1 = list1.GetEnumerator();
+        while (enumerator1.MoveNext())
         {
-            result.Add(item);
+            if (seen.Add(enumerator1.Current))
+                yield return enumerator1.Current;
         }
 
-        foreach (T item in list2)
+        using var enumerator2 = list2.GetEnumerator();
+        while (enumerator2.MoveNext())
         {
-            if(result.Contains(item))
-            {
-                continue;
-            }
-            result.Add(item);
+            if (seen.Add(enumerator2.Current))
+                yield return enumerator2.Current;
         }
-        return result;
+
+        //List<T> result = new List<T>();
+        //foreach (T item in list1)
+        //{
+        //    result.Add(item);
+        //}
+
+        //foreach (T item in list2)
+        //{
+        //    if(result.Contains(item))
+        //    {
+        //        continue;
+        //    }
+        //    result.Add(item);
+        //}
+        //return result;
     }
 
     public static IEnumerable<T> MyIntersect<T>(this IEnumerable<T> list1, IEnumerable<T> list2)
@@ -98,20 +160,33 @@ static class MyLinq
         ArgumentNullException.ThrowIfNull(list1, nameof(list1));
         ArgumentNullException.ThrowIfNull(list2, nameof(list2));
 
-        List<T> result = new List<T>();
+        HashSet<T> set2 = new HashSet<T>(list2);
+        HashSet<T> yielded = new HashSet<T>();
 
-        foreach (T item in list1)
+        using var enumerator1 = list1.GetEnumerator();
+        while (enumerator1.MoveNext())
         {
-            foreach (T item2 in list2)
+            var current = enumerator1.Current;
+            if (set2.Contains(current) && yielded.Add(current))
             {
-                if (item.Equals(item2))
-                {
-                    result.Add(item);
-                    break;
-                }
+                yield return current;
             }
         }
-        return result;
+
+        //List<T> result = new List<T>();
+
+        //foreach (T item in list1)
+        //{
+        //    foreach (T item2 in list2)
+        //    {
+        //        if (item.Equals(item2))
+        //        {
+        //            result.Add(item);
+        //            break;
+        //        }
+        //    }
+        //}
+        //return result;
     }
 
     public static IEnumerable<T> MyExcept<T>(this IEnumerable<T> list1, IEnumerable<T> list2)
@@ -119,62 +194,86 @@ static class MyLinq
         ArgumentNullException.ThrowIfNull(list1, nameof(list1));
         ArgumentNullException.ThrowIfNull(list2, nameof(list2));
 
-        List<T> result = new List<T>();
 
-        foreach (T item1 in list1)
+        HashSet<T> set2 = new HashSet<T>(list2);
+
+        using var enumerator1 = list1.GetEnumerator();
+        while (enumerator1.MoveNext())
         {
-            bool found = false;
-
-            foreach (var item2 in list2)
+            var current = enumerator1.Current;
+            if (!set2.Contains(current))
             {
-                if (item1!.Equals(item2))
-                {
-                    found = true;
-                    break;
-                }
+                yield return current;
             }
-            if (!found) result.Add(item1);
         }
 
-        return result;
+        //List<T> result = new List<T>();
+
+        //foreach (T item1 in list1)
+        //{
+        //    bool found = false;
+
+        //    foreach (var item2 in list2)
+        //    {
+        //        if (item1!.Equals(item2))
+        //        {
+        //            found = true;
+        //            break;
+        //        }
+        //    }
+        //    if (!found) result.Add(item1);
+        //}
+
+        //return result;
     }
 
     public static IEnumerable<T> MyDistinct<T>(this IEnumerable<T> source)
     {
         ArgumentNullException.ThrowIfNull(source, nameof(source));
 
-        List<T> result = new List<T>();
+        HashSet<T> seen = new HashSet<T>();
 
-        foreach(T item in source)
+        using var enumerator = source.GetEnumerator();
+        while (enumerator.MoveNext())
         {
-            if (!result.Contains(item))
-            {
-                result.Add(item);
-            }
+            if (seen.Add(enumerator.Current))
+                yield return enumerator.Current;
         }
-        return result;
+
+        //List<T> result = new List<T>();
+
+        //foreach(T item in source)
+        //{
+        //    if (!result.Contains(item))
+        //    {
+        //        result.Add(item);
+        //    }
+        //}
+        //return result;
     }
 
     public static IEnumerable<T> MySkip<T>(this IEnumerable<T> source, int count)
     {
         ArgumentNullException.ThrowIfNull(source, nameof(source));
-        if (count < 0)
-            throw new ArgumentOutOfRangeException(nameof(count), "Count cannot be negative.");
 
-        List<T> result = new List<T>();
+        using var enumerator = source.GetEnumerator();
+        int i = 0;
+        while (i < count && enumerator.MoveNext())
+            i++;
 
-        int index = 0;
+        while (enumerator.MoveNext())
+            yield return enumerator.Current;
 
-        foreach (T item in source)
-        {
-            if (index >= count)
-            {
-                result.Add(item);
-            }
-            index++;
-        }
-
-        return result;
+        //using var enumerator = source.GetEnumerator();
+        //int i = 0;
+        //while (i < count && enumerator.MoveNext())
+        //{
+        //    i++;
+        //}
+        //while (enumerator.MoveNext())
+        //{
+        //    yield return enumerator.Current;
+        //};
     }
 
     public static IEnumerable<T> MyTake<T>(this IEnumerable<T> source, int count)
@@ -183,19 +282,28 @@ static class MyLinq
         if (count < 0)
             throw new ArgumentOutOfRangeException(nameof(count), "Count cannot be negative.");
 
-        List<T> result = new List<T>();
+        using var enumerator = source.GetEnumerator();
+        int i = 0;
 
-        int index = 0;
-
-        foreach(T item in source)
+        while (i < count && enumerator.MoveNext())
         {
-            if (index < count)
-            {
-                result.Add(item);
-            index++;
-            }
+            yield return enumerator.Current;
+            i++;
         }
-        return result;
+
+        //List<T> result = new List<T>();
+
+        //int index = 0;
+
+        //foreach(T item in source)
+        //{
+        //    if (index < count)
+        //    {
+        //        result.Add(item);
+        //    index++;
+        //    }
+        //}
+        //return result;
     }
 
     public static IEnumerable<T> MyTakeWhile<T>(this IEnumerable<T> source, Func<T, bool> predicate)
@@ -203,16 +311,25 @@ static class MyLinq
         ArgumentNullException.ThrowIfNull(source, nameof(source));
         ArgumentNullException.ThrowIfNull(predicate, nameof(predicate));
 
-        foreach (T item in source)
+        using var enumerator = source.GetEnumerator();
+        while (enumerator.MoveNext())
         {
-            if (predicate(item))
-            {
-                yield return item;
-            }
+            if (predicate(enumerator.Current))
+                yield return enumerator.Current;
             else
-            {
                 yield break;
-            }
         }
+
+        //foreach (T item in source)
+        //{
+        //    if (predicate(item))
+        //    {
+        //        yield return item;
+        //    }
+        //    else
+        //    {
+        //        yield break;
+        //    }
+        //}
     }
 }
